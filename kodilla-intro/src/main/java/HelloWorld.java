0317cbdb4065a5451e363b2907b69a7332ab5fc6
Anna Lodyga <alodyga8@gmail.com>
2023-03-31 11:16:05 +0200

    Edited version

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String greeting= "Hello World";
+        String myName = "Anna";
+        int myAge = 33;
+        char mySurname = 'L';
+        System.out.println("greeting: "+greeting);
+        System.out.println("myName: "+myName);
+        System.out.println("myAge: "+myAge);
+        System.out.println("mySurname: "+mySurname);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String carName = "Volkswagen";
+        char bestGrade = 'A';
+        boolean isFunny = true;
+        float piNumber = 3.141592f;
+        int favNumber = 6;
+        double myHeight = 1.68;
+        System.out.println("carName: "+carName);
+        System.out.println("bestGrade: "+bestGrade);
+        System.out.println("isFunny: "+isFunny);
+        System.out.println("piNumber: "+piNumber);
+        System.out.println("favNumber: "+favNumber);
+        System.out.println("myHeight: "+myHeight);
+    }
+}
